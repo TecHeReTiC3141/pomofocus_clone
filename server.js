f91b7b3a224386069a4c2e1fc406699439011a32
TecHeReTiC <talex73@mail.ru>
2023-06-26 21:58:24 +0500

    Project structure has been created and some dependencies have been installed

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
-
 const app = express();
+
+const methodOverride = require('method-override');
+
+const indexRouter = require('./routes/index');
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb",
+    extended: true, parameterLimit: 50000 }))
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+
+app.use('/', indexRouter);
 
 app.listen(process.env.POST || 3000,
     () => console.log('On http://localhost:3000'));
